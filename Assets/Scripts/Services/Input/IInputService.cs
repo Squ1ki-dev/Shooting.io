423a69fd5c163adcc5dc66244dd25155d637ce52
Squ1ki <devunit46@gmail.com>
2024-12-20 16:25:06 +0000

    Initial commit

---
+++ Assets/Scripts/Services/Input/IInputService.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+public interface IInputService
+{
+    Vector2 Axis { get; }
+}
---
+++ Assets/Scripts/Services/Input/StandaloneInputService.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class StandaloneInputService : InputService
+{
+    public override Vector2 Axis 
+    {
+        get
+        {
+            Vector2 axis = SimpleInputAxis();
+
+            if(axis == Vector2.zero)
+                axis = UnityAxis();
+
+            return axis;
+        } 
+    }
+
+    private static Vector2 UnityAxis() => new Vector2(SimpleInput.GetAxis(Horizontal), SimpleInput.GetAxis(Vertical));
+}
