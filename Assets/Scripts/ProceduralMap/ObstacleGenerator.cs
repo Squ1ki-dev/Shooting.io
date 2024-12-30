5b0a831c20cc6aa23dbbfdd58a7df271bb980c1f
Squ1ki <devunit46@gmail.com>
2024-12-30 16:04:35 +0000

    fix

--- Assets/Scripts/ProceduralMap/ObstacleGenerator.cs
+++ Assets/Scripts/ProceduralMap/ObstacleGenerator.cs
@@ -13,6 +13,8 @@ namespace CodeBase.ProceduralMap
 
         [SerializeField] private float itemXSpread, itemZSpread;
 
+        private int itemsSpawned = 0;
+
         private void Start()
         {
             for (int i = 0; i < numItemsToSpawn; i++)
@@ -29,13 +31,30 @@ namespace CodeBase.ProceduralMap
             assetReference.InstantiateAsync(SpreadItems(), Quaternion.identity, parentObject).Completed += OnItemInstantiated;
         }
 
-        private Vector3 SpreadItems() =>
-            new Vector3(Random.Range(-itemXSpread, itemXSpread), 0, Random.Range(-itemZSpread, itemZSpread)) + transform.position;
-
         private void OnItemInstantiated(AsyncOperationHandle<GameObject> obj)
         {
             if (obj.Status != AsyncOperationStatus.Succeeded)
+            {
                 Debug.LogError("Failed to load item.");
+                return;
+            }
+
+            itemsSpawned++;
+
+            if (itemsSpawned == numItemsToSpawn)
+                CombineMeshes();
         }
+
+        private void CombineMeshes()
+        {
+            MeshCombiner meshCombiner = parentObject.gameObject.AddComponent<MeshCombiner>();
+
+            meshCombiner.CreateMultiMaterialMesh = true;
+            meshCombiner.DestroyCombinedChildren = true;
+            meshCombiner.CombineMeshes(false);
+        }
+
+        private Vector3 SpreadItems() =>
+            new Vector3(Random.Range(-itemXSpread, itemXSpread), 0, Random.Range(-itemZSpread, itemZSpread)) + transform.position;
     }
 }
