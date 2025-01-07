881e323d5ffd2eb98c34613701743ae8f8b084bb
Squ1ki <devunit46@gmail.com>
2025-01-07 12:14:32 +0000

    level fix, balance the game

--- Assets/Scripts/UI/Elements/LevelBar.cs
+++ Assets/Scripts/UI/Elements/LevelBar.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 using TMPro;
 using CodeBase.Enemy;
 using CodeBase.Player;
+using CodeBase.Service;
 
 namespace CodeBase.UI.Elements
 {
@@ -34,8 +35,8 @@ namespace CodeBase.UI.Elements
                 PlayerPrefs.Save();
             }
 
-            _levelText.text = "Level: " + PlayerPrefs.GetInt(Constants.Level);
             playerConfig.Level = PlayerPrefs.GetInt(Constants.Level);
+            _levelText.text = "Level: " + playerConfig.Level.ToString();
 
             if (PlayerPrefs.HasKey(Constants.FillAmount))
             {
@@ -46,6 +47,15 @@ namespace CodeBase.UI.Elements
             {
                 _imageCurrent.fillAmount = 0;
                 _currentXP = 0;
+            }
+
+            if (PlayerPrefs.HasKey(Constants.TargetXP))
+                _targetXP = PlayerPrefs.GetInt(Constants.TargetXP);
+            else
+            {
+                _targetXP = 100;
+                PlayerPrefs.SetInt(Constants.TargetXP, _targetXP);
+                PlayerPrefs.Save();
             }
 
             Debug.Log("Level " + PlayerPrefs.GetInt(Constants.Level));
@@ -70,6 +80,8 @@ namespace CodeBase.UI.Elements
                 _currentXP -= _targetXP;
                 playerConfig.Level++;
                 PlayerPrefs.SetInt(Constants.Level, playerConfig.Level);
+                PlayerStatsService.SavePlayerStats(playerConfig);
+                _levelText.text = "Level: " + playerConfig.Level.ToString();
                 Debug.Log($"Level increased to: {playerConfig.Level}");
                 _mainGame.ChangeState(GameStates.Upgrade);
 
